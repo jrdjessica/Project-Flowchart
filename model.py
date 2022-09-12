9dcd18f344e1b74551316a499c949c852051ae19
Jessica Dea <jessicardea@gmail.com>
2022-09-12 14:58:40 -0700

    Customer class

--- model.py
+++ model.py
@@ -16,3 +16,20 @@ class User(db.Model):
     def __repr__(self):
 
         return f'<User user_id={self.user_id} email={self.email}>'
+
+
+class Customer(db.Model):
+
+    __tablename__ = 'customers'
+
+    customer_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    fname = db.Column(db.String)
+    lname = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+    country = db.Column(db.String)
+
+    def __repr__(self):
+
+        return f'<Customer customer_id={self.customer_id}> fname={self.fname}'
