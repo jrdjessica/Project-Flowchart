df7b513da48cd12f7ed5d96abeb45ec0112d3f77
Jessica Dea <jessicardea@gmail.com>
2022-09-12 15:07:01 -0700

    Relationships between classes

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
+    """A user."""
 
     __tablename__ = 'users'
 
@@ -14,12 +15,15 @@ class User(db.Model):
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
 
+    customer = db.relationship('Customer', back_populates='user')
+
     def __repr__(self):
 
         return f'<User user_id={self.user_id} email={self.email}>'
 
 
 class Customer(db.Model):
+    """A customer."""
 
     __tablename__ = 'customers'
 
@@ -31,18 +35,27 @@ class Customer(db.Model):
     state = db.Column(db.String)
     country = db.Column(db.String)
 
+    user = db.relationship('User', back_populates='customer')
+    order = db.relationship('Order', back_populates='customer')
+
     def __repr__(self):
 
         return f'<Customer customer_id={self.customer_id}> fname={self.fname}'
 
 
 class Order(db.Model):
+    """An order."""
+
+    __tablename__ = 'orders'
+
     order_id = db.Column(db.String, primary_key=True)
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.user_id'))
     num_items = db.Column(db.Integer)
     date = db.Column(db.DateTime)
     total = db.Column(db.Integer)
     net = db.Column(db.Integer)
+
+    customer = db.relationship('Customer', back_populates='order')
 
     def __repr__(self):
 
