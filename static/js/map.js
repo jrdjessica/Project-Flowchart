fe96e5c3db99755f1b1e64e54138a4c1c2bde62b
Jessica Dea <jessicardea@gmail.com>
2022-09-15 11:39:10 -0700

    Add marker to map

--- static/js/map.js
+++ static/js/map.js
@@ -1,14 +1,30 @@
+let address1 = 'Mooregate Trail, Hawthorn Woods, IL, United States'
+let address2 = 'Ethel Street, Cambridge, ON, Canada'
+let address3 = 'Goeman Borgesiusstraat 229, Nijmegen, The Netherlands'
+
+// Initialize map, starts in US
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru,
+    geocoder = new google.maps.Geocoder();
+
+    const bangalore = { lat: 12.97, lng: 77.59 };
+    const start = { lat: 39.828, lng: -98.579 };
+
+    const map = new google.maps.Map(document.querySelector('#map'), {
+        zoom: 1,
+        center: start,
     });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-        position: uluru,
+
+    addMarker(bangalore, map);
+    codeAddress(address1, map);
+    codeAddress(address2, map);
+    codeAddress(address3, map);
+}
+
+// Add markers to map
+function addMarker(location, map) {
+    new google.maps.Marker({
+        position: location,
         map: map,
     });
 }
+
