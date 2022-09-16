57a64f0844f07464bdc05228742d6fe124e9df0d
Jessica Dea <jessicardea@gmail.com>
2022-09-15 17:23:32 -0700

    Geocode individual addresses

--- static/js/map.js
+++ static/js/map.js
@@ -1,23 +1,14 @@
-let address1 = 'Mooregate Trail, Hawthorn Woods, IL, United States'
-let address2 = 'Ethel Street, Cambridge, ON, Canada'
-let address3 = 'Goeman Borgesiusstraat 229, Nijmegen, The Netherlands'
 
 // Initialize map, starts in US
 function initMap() {
     geocoder = new google.maps.Geocoder();
 
-    const bangalore = { lat: 12.97, lng: 77.59 };
     const start = { lat: 39.828, lng: -98.579 };
 
     const map = new google.maps.Map(document.querySelector('#map'), {
         zoom: 1,
         center: start,
     });
-
-    addMarker(bangalore, map);
-    codeAddress(address1, map);
-    codeAddress(address2, map);
-    codeAddress(address3, map);
 
     fetch('/api/shop')
         .then(res => res.json())
@@ -33,7 +24,7 @@ function initMap() {
                         }
                     }
                 }
-                console.log(address);
+                codeAddress(address, map)
             }
         }
         )
@@ -57,12 +48,10 @@ function codeAddress(address, map) {
             let latitude = results[0].geometry.location.lat();
             let longitude = results[0].geometry.location.lng();
 
-            let marker = new google.maps.Marker({
-                position: { lat: latitude, lng: longitude },
-                map: map
-            })
+            addMarker({ lat: latitude, lng: longitude }, map);
+
         } else {
-            alert('Address was unsuccessful.');
+            alert(`Address was unsuccessful. ${address} ${status}`);
         }
     });
 }
