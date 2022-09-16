60d02a24cdd37278d5530838565c43f959a323e7
Jessica Dea <jessicardea@gmail.com>
2022-09-16 15:00:03 -0700

    Added info window

--- static/js/map.js
+++ static/js/map.js
@@ -1,7 +1,6 @@
 
 // Initialize map, starts in US
 function initMap() {
-    geocoder = new google.maps.Geocoder();
 
     const start = { lat: 39.828, lng: -98.579 };
 
@@ -14,9 +13,12 @@ function initMap() {
         .then(res => res.json())
         .then(orders => {
             for (let order of orders) {
-                lat = order.latitude
-                lng = order.longitude
-                addMarker({ lat: lat, lng: lng }, map)
+                lat = order.latitude;
+                lng = order.longitude;
+
+                addMarker({ lat: lat, lng: lng }, map);
+                // addInfoWindow(map);
+                // console.log(order)
             }
         }
         )
@@ -25,14 +27,33 @@ function initMap() {
 
 // Add markers to map
 function addMarker(location, map) {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
         position: location,
         map: map,
     });
+    addInfoWindow(marker, map)
 }
+
+// Add info window
+function addInfoWindow(marker, map) {
+    const infowindow = new google.maps.InfoWindow({
+        content: 'contentString',
+    });
+
+    marker.addListener("click", () => {
+        infowindow.open({
+            map: map,
+            anchor: marker,
+        });
+    });
+}
+
+
+
 
 // Maps javascript api - geocoding
 // Takes in address, returns lat and lng, add to map
+// geocoder = new google.maps.Geocoder();
 // function codeAddress(address, map) {
 //     geocoder.geocode({ 'address': address }, function (results, status) {
 
