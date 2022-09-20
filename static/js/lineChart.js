195a6f935866b09ebb57ca9edbd802bb8fff349d
Jessica Dea <jessicardea@gmail.com>
2022-09-20 15:14:30 -0700

    Line chart sort orders by date

--- static/js/lineChart.js
+++ static/js/lineChart.js
@@ -17,27 +17,30 @@ function drawChart() {
             let net_total = 0;
             let order_total = 0;
 
-            // Reverse loop through array for correct date order
-            for (let i = orders.length - 1; i >= 0; i--) {
-                order_total += orders[i].total;
-                net_total += orders[i].net;
+            // Sort orders by date
+            const sortedOrder = orders.sort((a, b) => new Date(a.date) - new Date(b.date));
 
-                let addRow = [new Date(orders[i].date), net_total, order_total];
+            // Reverse loop through array for correct date order
+            for (let i = 0; i < sortedOrder.length; i++) {
+                order_total += sortedOrder[i].total;
+                net_total += sortedOrder[i].net;
+
+                let addRow = [new Date(sortedOrder[i].date), net_total, order_total];
 
                 chartRow.push(addRow);
             }
 
+
             data.addColumn('datetime', 'Time');
             data.addColumn('number', 'Net Order in Dollars');
             data.addColumn('number', 'Total Order in Dollars');
+
 
             for (let row of chartRow) {
                 data.addRows([
                     row
                 ]);
             }
-            console.log(orders);
-            console.log(chartRow);
 
             var options = {
                 chart: {
