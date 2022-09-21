973a01d2c05c0729e79959e246f3f7efa6de25d1
Jessica Dea <jessicardea@gmail.com>
2022-09-21 12:42:41 -0700

    Add rows from dataset

--- static/js/calendarChart.js
+++ static/js/calendarChart.js
@@ -23,54 +23,39 @@ function drawChart() {
 
                     if (row['dateValue'] == new Date(orders[i].date).getTime()) {
                         row['numItems'] += orders[i].num_items;
-                        // console.log(i);
-                        // console.log(row['dateValue']);
-                        // console.log(new Date(orders[29].date).getTime());
                         break
                     }
                     else if (row == lastItem) {
-                        let addRow = { date: orders[i].date, dateValue: new Date(orders[i].date).getTime(), numItems: orders[i].num_items };
+                        let addRow = { date: new Date(orders[i].date), dateValue: new Date(orders[i].date).getTime(), numItems: orders[i].num_items };
                         chartRows.push(addRow);
-                        // console.log('hi')
                         break
                     }
                 }
-                // console.log('****');
-                // let addRow = { date: orders[i].date, dateValue: new Date(orders[i].date).getTime(), numItems: orders[i].num_items };
-                // chartRows.push(addRow);
-                // console.log(chartRows);
-                // console.log(i);
+
             }
             console.log(chartRows);
-
-            // console.log(chartRows[6]['dateValue']);
-            // console.log(new Date(orders[7].date).getTime());
-
-            // if (chartRows[9]['dateValue'] == new Date(orders[10].date).getTime()) {
-            //     console.log('iiiiiiiii')
-            // }
-
 
             dataTable.addColumn({ type: 'date', id: 'Date' });
             dataTable.addColumn({ type: 'number', id: 'Won/Loss' });
 
-            dataTable.addRows([
-                [new Date(2012, 3, 13), 37032],
-                [new Date(2012, 3, 14), 38024],
-                [new Date(2012, 3, 15), 38024],
-                [new Date(2012, 3, 16), 38108],
-                [new Date(2012, 3, 17), 38229],
-                // Many rows omitted for brevity.
-                [new Date(2013, 9, 4), 38177],
-                [new Date(2012, 4, 17), 38229],
-                [new Date(2013, 9, 5), 38705],
-                [new Date(2013, 9, 12), 38210],
-                [new Date(2013, 9, 13), 38029],
-                [new Date(2013, 9, 19), 38823],
-                [new Date(2013, 9, 23), 38345],
-                [new Date(2013, 9, 24), 38436],
-                [new Date(2013, 9, 30), 38447]
-            ]);
+
+            // console.log(chartRows);
+
+            for (let row of chartRows) {
+                dataTable.addRows([
+                    [row['date'], row['numItems']]
+                ]);
+            }
+            // let addrow = [];
+            // for (let row of chartRows) {
+
+            //     let rows = [row['date'], row['numItems']];
+            //     addrow.push(rows);
+
+
+            // }
+            // console.log(addrow);
+
 
             var chart = new google.visualization.Calendar(document.querySelector('#calendar_basic'));
 
