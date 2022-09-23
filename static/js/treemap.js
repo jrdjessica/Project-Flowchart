0012f3307222554a13b5f60b1da55a7d9a674d4c
Jessica Dea <jessicardea@gmail.com>
2022-09-23 15:40:53 -0700

    Added city parent data

--- static/js/treemap.js
+++ static/js/treemap.js
@@ -7,7 +7,6 @@ function drawChart() {
     fetch('/api/shop')
         .then(res => res.json())
         .then(orders => {
-            let rowCharts = [];
 
             // Add country parent ['United States', 'Global', 0]
             let rowCountry = [[orders[0].country, 'Global', 0]];
@@ -20,11 +19,6 @@ function drawChart() {
 
 
             for (let order of orders) {
-                // console.log(order.country);
-                // let addRowCity = [order.city, order.state, order.num_items];
-
-
-                let lastItem = rowCountry[rowCountry.length - 1];
 
                 // Add unique country parent row
                 for (let row of rowCountry) {
@@ -33,7 +27,7 @@ function drawChart() {
                     if (JSON.stringify(row) === JSON.stringify(addRowCountry)) {
                         break
                     }
-                    else if (row === lastItem) {
+                    else if (row === rowCountry[rowCountry.length - 1]) {
                         rowCountry.push(addRowCountry);
                         break
                     }
@@ -54,39 +48,23 @@ function drawChart() {
                         rowState.push(addRowState);
                         break
                     }
-
-
-                    for (let row of rowCity) {
-                        let addRowCity = [order.city, order.state, order.num_items];
-                        if (row[0] == order.city) {
-                            row[2] += order.num_items;
-                            // console.log(order.city);
-                            break
-                        }
-                        else if (row == rowCity[rowCity.length - 1]) {
-                            rowCity.push(addRowCity);
-                            // console.log(order.city);
-                            break
-                        }
-                    }
-
                 }
-                // console.log([order.state, order.country, order.num_items]);
 
+                for (let row of rowCity) {
+                    let addRowCity = [order.city, order.state, order.num_items];
+                    if (row[0] == order.city) {
+                        row[2] += order.num_items;
+                        break
+                    }
+                    else if (row == rowCity[rowCity.length - 1]) {
+                        rowCity.push(addRowCity);
+                        break
+                    }
+                }
 
             }
-            // console.log(rowCharts);
 
-            // console.log(rowCountry);
             console.log(rowState);
-            // console.log(rowCity);
-
-            rowCharts.push(rowCountry);
-            rowCharts.push(rowState);
-            // console.log(rowCharts);
-
-
-
             const data = new google.visualization.DataTable();
 
             data.addColumn('string', 'Location');
@@ -106,64 +84,12 @@ function drawChart() {
                 ]);
             }
 
-            // data.addRows([
-            //     ['', 'The Netherlands', 11]
-            // ]);
+            for (let row of rowCity) {
+                data.addRows([
+                    row
+                ]);
+            }
 
-
-
-
-            // const data = google.visualization.arrayToDataTable([
-
-            // ['Location', 'Parent', 'num orders']
-
-            // ['Global', null, 0, 0],
-
-            // country, parent, num orders
-            // ['Brazil', 'Global', 0],
-            // ['United States', 'Global', 0],
-            // ['Mexico', 'Global', 0],
-            // ['Canada', 'Global', 0],
-
-            // state, country, num orders
-            // ['CA', 'United States', 11],
-            // ['ON', 'Canada', 52],
-
-            // city, state, num orders
-            // ['Millcreek', 'UT', 11],
-            // ['Raleigh', 'NC', 52],
-
-            //     ['Location', 'Parent', 'Market trade volume (size)', 'Market increase/decrease (color)'],
-            //     ['Global', null, 0, 0],
-            //     ['America', 'Global', 0, 0],
-            //     ['Europe', 'Global', 0, 0],
-            //     ['Asia', 'Global', 0, 0],
-            //     ['Australia', 'Global', 0, 0],
-            //     ['Africa', 'Global', 0, 0],
-            //     ['Brazil', 'America', 11, 10],
-            //     ['USA', 'America', 52, 31],
-            //     ['Mexico', 'America', 24, 12],
-            //     ['Canada', 'America', 16, -23],
-            //     ['France', 'Europe', 42, -11],
-            //     ['Germany', 'Europe', 31, -2],
-            //     ['Sweden', 'Europe', 22, -13],
-            //     ['Italy', 'Europe', 17, 4],
-            //     ['UK', 'Europe', 21, -5],
-            //     ['China', 'Asia', 36, 4],
-            //     ['Japan', 'Asia', 20, -12],
-            //     ['India', 'Asia', 40, 63],
-            //     ['Laos', 'Asia', 4, 34],
-            //     ['Mongolia', 'Asia', 1, -5],
-            //     ['Israel', 'Asia', 12, 24],
-            //     ['Iran', 'Asia', 18, 13],
-            //     ['Pakistan', 'Asia', 11, -52],
-            //     ['Egypt', 'Africa', 21, 0],
-            //     ['S. Africa', 'Africa', 30, 43],
-            //     ['Sudan', 'Africa', 12, 2],
-            //     ['Congo', 'Africa', 10, 12],
-            //     ['Zaire', 'Africa', 8, 10]
-            //     ['CA', 'USA', 12, 11],
-            // ]);
 
             const tree = new google.visualization.TreeMap(document.querySelector('#treemap'));
 
