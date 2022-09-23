6b8eb9ad2909a86db0a0e3ba632bb2f54193a589
Jessica Dea <jessicardea@gmail.com>
2022-09-23 14:39:45 -0700

    Added country and state parents to chart

--- static/js/treemap.js
+++ static/js/treemap.js
@@ -32,6 +32,7 @@ function drawChart() {
                     }
                     else if (row === lastItem) {
                         rowCountry.push(addRowCountry);
+                        break
                     }
                 }
 
@@ -47,6 +48,12 @@ function drawChart() {
                         break
                     }
 
+
+                    // for (let row of rowState) {
+                    //     let addRowCity = [order.city, order.state, order.num_items];
+
+                    // }
+
                 }
                 // console.log([order.state, order.country, order.num_items]);
 
@@ -57,57 +64,82 @@ function drawChart() {
             console.log(rowCountry);
             console.log(rowState);
 
+            rowCharts.push(rowCountry);
+            rowCharts.push(rowState);
+            console.log(rowCharts);
 
-            const data = google.visualization.arrayToDataTable([
 
-                // ['Location', 'Parent', 'num orders']
 
-                // ['Global', null, 0, 0],
+            const data = new google.visualization.DataTable();
 
-                // country, parent, num orders
-                // ['Brazil', 'Global', 0],
-                // ['United States', 'Global', 0],
-                // ['Mexico', 'Global', 0],
-                // ['Canada', 'Global', 0],
+            data.addColumn('string', 'Location');
+            data.addColumn('string', 'Parent');
+            data.addColumn('number', 'Num Orders');
 
-                // state, country, num orders
-                // ['CA', 'United States', 11],
-                // ['ON', 'Canada', 52],
+            for (let row of rowCountry) {
+                data.addRows([
+                    ['Global', null, 0],
+                    row
+                ]);
+            }
 
-                // city, state, num orders
-                // ['Millcreek', 'UT', 11],
-                // ['Raleigh', 'NC', 52],
+            for (let row of rowState) {
+                data.addRows([
+                    row
+                ]);
+            }
 
-                ['Location', 'Parent', 'Market trade volume (size)', 'Market increase/decrease (color)'],
-                ['Global', null, 0, 0],
-                ['America', 'Global', 0, 0],
-                ['Europe', 'Global', 0, 0],
-                ['Asia', 'Global', 0, 0],
-                ['Australia', 'Global', 0, 0],
-                ['Africa', 'Global', 0, 0],
-                ['Brazil', 'America', 11, 10],
-                ['USA', 'America', 52, 31],
-                ['Mexico', 'America', 24, 12],
-                ['Canada', 'America', 16, -23],
-                ['France', 'Europe', 42, -11],
-                ['Germany', 'Europe', 31, -2],
-                ['Sweden', 'Europe', 22, -13],
-                ['Italy', 'Europe', 17, 4],
-                ['UK', 'Europe', 21, -5],
-                ['China', 'Asia', 36, 4],
-                ['Japan', 'Asia', 20, -12],
-                ['India', 'Asia', 40, 63],
-                ['Laos', 'Asia', 4, 34],
-                ['Mongolia', 'Asia', 1, -5],
-                ['Israel', 'Asia', 12, 24],
-                ['Iran', 'Asia', 18, 13],
-                ['Pakistan', 'Asia', 11, -52],
-                ['Egypt', 'Africa', 21, 0],
-                ['S. Africa', 'Africa', 30, 43],
-                ['Sudan', 'Africa', 12, 2],
-                ['Congo', 'Africa', 10, 12],
-                ['Zaire', 'Africa', 8, 10]
-            ]);
+            // const data = google.visualization.arrayToDataTable([
+
+            // ['Location', 'Parent', 'num orders']
+
+            // ['Global', null, 0, 0],
+
+            // country, parent, num orders
+            // ['Brazil', 'Global', 0],
+            // ['United States', 'Global', 0],
+            // ['Mexico', 'Global', 0],
+            // ['Canada', 'Global', 0],
+
+            // state, country, num orders
+            // ['CA', 'United States', 11],
+            // ['ON', 'Canada', 52],
+
+            // city, state, num orders
+            // ['Millcreek', 'UT', 11],
+            // ['Raleigh', 'NC', 52],
+
+            //     ['Location', 'Parent', 'Market trade volume (size)', 'Market increase/decrease (color)'],
+            //     ['Global', null, 0, 0],
+            //     ['America', 'Global', 0, 0],
+            //     ['Europe', 'Global', 0, 0],
+            //     ['Asia', 'Global', 0, 0],
+            //     ['Australia', 'Global', 0, 0],
+            //     ['Africa', 'Global', 0, 0],
+            //     ['Brazil', 'America', 11, 10],
+            //     ['USA', 'America', 52, 31],
+            //     ['Mexico', 'America', 24, 12],
+            //     ['Canada', 'America', 16, -23],
+            //     ['France', 'Europe', 42, -11],
+            //     ['Germany', 'Europe', 31, -2],
+            //     ['Sweden', 'Europe', 22, -13],
+            //     ['Italy', 'Europe', 17, 4],
+            //     ['UK', 'Europe', 21, -5],
+            //     ['China', 'Asia', 36, 4],
+            //     ['Japan', 'Asia', 20, -12],
+            //     ['India', 'Asia', 40, 63],
+            //     ['Laos', 'Asia', 4, 34],
+            //     ['Mongolia', 'Asia', 1, -5],
+            //     ['Israel', 'Asia', 12, 24],
+            //     ['Iran', 'Asia', 18, 13],
+            //     ['Pakistan', 'Asia', 11, -52],
+            //     ['Egypt', 'Africa', 21, 0],
+            //     ['S. Africa', 'Africa', 30, 43],
+            //     ['Sudan', 'Africa', 12, 2],
+            //     ['Congo', 'Africa', 10, 12],
+            //     ['Zaire', 'Africa', 8, 10]
+            //     ['CA', 'USA', 12, 11],
+            // ]);
 
             const tree = new google.visualization.TreeMap(document.querySelector('#treemap'));
 
