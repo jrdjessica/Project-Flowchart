8a3975291bfed25aadfd1fc70c78578664330720
Jessica Dea <jessicardea@gmail.com>
2022-09-29 14:04:57 -0700

    Added number of orders and countries to react comp

--- static/js/dashboard.jsx
+++ static/js/dashboard.jsx
@@ -4,6 +4,14 @@ function DashboardContainer() {
             .then(res => res.json())
             .then(orders => {
                 console.log(orders);
+
+                const numOrders = orders.length;
+
+                const countries = new Set();
+                for (let i in orders) {
+                    countries.add(orders[i].country);
+                }
+
             });
     }, []);
     return (
