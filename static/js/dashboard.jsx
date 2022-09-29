a580b9e0c943651510a1301da00328ac861dd9f3
Jessica Dea <jessicardea@gmail.com>
2022-09-29 14:17:26 -0700

    Average and total order value

--- static/js/dashboard.jsx
+++ static/js/dashboard.jsx
@@ -5,12 +5,30 @@ function DashboardContainer() {
             .then(orders => {
                 console.log(orders);
 
+                // Number of orders
                 const numOrders = orders.length;
 
+
                 const countries = new Set();
+                const totalOrderValue = [];
+
                 for (let i in orders) {
                     countries.add(orders[i].country);
+                    totalOrderValue.push(orders[i].total);
                 }
+
+                // Number of countries
+                const numCountries = countries.size;
+
+                // Average order value and total order value
+                let totalValue = 0;
+                for (let val of totalOrderValue) {
+                    totalValue += val;
+                }
+                const avgValue = totalValue / numOrders
+
+
+
 
             });
     }, []);
