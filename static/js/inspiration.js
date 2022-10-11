decdbdc781b26799c4c8a4bb3900c7ee6650b60a
Jessica Dea <jessicardea@gmail.com>
2022-10-10 19:22:41 -0700

    Display images in 2 columns

--- static/js/inspiration.js
+++ static/js/inspiration.js
@@ -4,3 +4,12 @@ function clickWord(e) {
     document.querySelector('#search').value = e;
 }
 
+let images = document.getElementsByClassName("column");
+
+
+function displayImages() {
+    for (let i = 0; i < images.length; i += 1) {
+        images[i].style.flex = "50%";
+    }
+}
+displayImages()
